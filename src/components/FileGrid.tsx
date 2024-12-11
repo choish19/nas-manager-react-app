cbfccbf0512d1bd38d9b7fcf280fc1f486b757ea
choish19 <100904838+choish19@users.noreply.github.com>
2024-12-11 13:37:12 +0900

    [Fix] FileGrid 배열 4열에서 5열로 변경

--- src/components/FileGrid.tsx
+++ src/components/FileGrid.tsx
@@ -11,7 +11,7 @@ interface FileGridProps {
 
 const FileGrid: React.FC<FileGridProps> = ({ files, onFileSelect, onToggleBookmark }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pr-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 pr-2">
       {files.map((file) => (
         <motion.div
           key={file.id}
